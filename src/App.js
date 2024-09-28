67068b8bf78588c6181a3e39220c094bc3b5de6f
ebans1904 <beans2853@gmail.com>
2024-09-28 23:17:48 +0700

    Router set

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { useTranslation } from 'react-i18next';
 import './App.css';
+import Main from './components/Main';
+import './util/i18n.ts';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Questions from './components/Questions.js';
+import Types from './components/Types.js';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const [lan, setLan] = useState("ko");
+  const handleLanChange = (lan) => {
+    setLan(lan);
+    localStorage.setItem("Language", lan);
+  }
+  useEffect(() => {
+    if (localStorage.getItem("Language")) {
+      setLan(localStorage.getItem("Language"));
+    }
+    i18n.changeLanguage(lan);
+  }, [lan]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main t={t} setLan={handleLanChange} />} />
+        <Route path='/test' element={<Questions t={t} />} />
+        <Route path='/types' element={<Types t={t} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
